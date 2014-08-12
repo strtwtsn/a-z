49f22bd076ed651fc846f062082af3d10c5de5d3
strtwtsn <stuart.watson@unep-wcmc.org>
2014-08-12 14:05:10 +0100

    Update default.rb

--- cookbooks/wkhtmlpdf/recipes/default.rb
+++ cookbooks/wkhtmlpdf/recipes/default.rb
@@ -1,13 +1,13 @@
 remote_file "Download wkhtml" do
-path "/home/ubuntu/downloads/wkhtmltox-0.12.1_linux-trusty-i386.deb"
-source "http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_linux-trusty-i386.deb"
+path "/home/ubuntu/downloads/wkhtmltox-0.12.1_linux-trusty-amd64.deb"
+source "http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb"
 end
 
 bash "Install wkhtml" do
 user "root"
 code <<-EOH
 cd /home/ubuntu/downloads
-dpkg -i wkhtmltox-0.12.1_linux-trusty-i386.deb
+dpkg -i wkhtmltox-0.12.1_linux-trusty-amd64.deb
 EOH
 end
 
