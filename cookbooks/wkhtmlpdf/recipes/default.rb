dd98934502f07b1c322f4440aae654b3a0879d21
Stuart Watson <stuart.watson@unep-wcmc.org>
2014-08-12 13:57:34 +0100

    Fixed problem with cookbook

--- cookbooks/wkhtmlpdf/recipes/default.rb
+++ cookbooks/wkhtmlpdf/recipes/default.rb
@@ -3,7 +3,7 @@ path "/home/ubuntu/downloads/wkhtmltox-0.12.1_linux-trusty-i386.deb"
 source "http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_linux-trusty-i386.deb"
 end
 
-bash "Install wkhtml"
+bash "Install wkhtml" do
 user "root"
 code <<-EOH
 cd /home/ubuntu/downloads
