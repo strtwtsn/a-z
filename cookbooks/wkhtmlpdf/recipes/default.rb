4d425e4ab532fb5520915345193c049b4b812c0c
Stuart Watson <stuart.watson@unep-wcmc.org>
2014-08-12 13:52:58 +0100

    Updated wkhtml cookbook

--- cookbooks/wkhtmlpdf/recipes/default.rb
+++ cookbooks/wkhtmlpdf/recipes/default.rb
@@ -1,7 +1,14 @@
-bash "Install wkhtmltopdf" do
+remote_file "Download wkhtml" do
+path "/home/ubuntu/downloads/wkhtmltox-0.12.1_linux-trusty-i386.deb"
+source "http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_linux-trusty-i386.deb"
+end
+
+bash "Install wkhtml"
+user "root"
 code <<-EOH
-git clone --recursive https://github.com/wkhtmltopdf/wkhtmltopdf.git /home/ubuntu/wkhtmltopdf
-cd /home/ubuntu/wkhtmltopdf
-scripts/build.py trusty-i386
+cd /home/ubuntu/downloads
+dpkg -i wkhtmltox-0.12.1_linux-trusty-i386.deb
 EOH
 end
+
+
