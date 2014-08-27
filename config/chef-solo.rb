e2971305d820d29c87f506aabae4d833aa3d604e
Stuart Watson <stuart.watson@unep-wcmc.org>
2014-08-27 10:21:41 +0100

    Removed passenger cookbook

--- config/chef-solo.rb
+++ config/chef-solo.rb
@@ -1,5 +1,5 @@
 file_cache_path "/tmp/chef-solo"
-cookbook_path "/var/chef/cookbooks"
+cookbook_path "/home/stuart/chef/a-z/cookbooks"
 log_level :info
 log_location STDOUT
 ssl_verify_mode :verify_none
--- cookbooks/passenger/recipes/default.rb
+++
@@ -1,6 +0,0 @@
-gem_package "passenger" do
-action :install
-end
-
-cd /home/ubuntu/.rvm/gems/ruby-1.9.3-p429/gems/passenger-4.0.5
-rvmsudo rake nginx
